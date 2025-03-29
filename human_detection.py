ba181e614e585e6b96b1cc018f1e397c1ca63701
Jamesrsh8 <144559513+Jamesrsh8@users.noreply.github.com>
2025-03-29 13:07:41 -0500

    queue_calculator working concurrently in human_detection

--- human_detection.py
+++ human_detection.py
@@ -1,59 +1,83 @@
 import numpy as np
 import cv2
- 
-# initialize the HOG descriptor/person detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-cv2.startWindowThread()
+from queue_calculator import Queue_Calculator
+import time
+import multiprocessing
 
-# open webcam video stream
-cap = cv2.VideoCapture(0)
+########## Our "main" file ################
 
-# the output will be written to output.avi
-# out = cv2.VideoWriter(
-#     'output.avi',
-#     cv2.VideoWriter_fourcc(*'MJPG'),
-#     15.,
-#     (640,480))
+def main():
+    # initialize the HOG descriptor/person detector
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-persons_detected = 0 
+    cv2.startWindowThread()
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    # open webcam video stream
+    cap = cv2.VideoCapture(0)
 
-    # resizing for faster detection
-    frame = cv2.resize(frame, (640, 480))
-    # using a greyscale picture, also for faster detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # the output will be written to output.avi
+    # out = cv2.VideoWriter(
+    #     'output.avi',
+    #     cv2.VideoWriter_fourcc(*'MJPG'),
+    #     15.,
+    #     (640,480))
 
-    # detect people in the image
-    # returns the bounding boxes for the detected objects
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+    # persons_detected becomes shared value across processes
+    persons_detected = multiprocessing.Value('i', 0)
 
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+    # refresh queue counting 5 times/sec, 10 min estimated per person
+    queue_tracker = Queue_Calculator(5, 10)
+    # pass in persons_detected so its available to queue_tracker object
+    process = multiprocessing.Process(target=queue_tracker.start_tracking, args=(persons_detected,))
+    process.start()
 
-    persons_detected = len(boxes)
-    print("Persons detected:", persons_detected)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    for (xA, yA, xB, yB) in boxes:
-        # display the detected boxes in the colour picture
-        cv2.rectangle(frame, (xA, yA), (xB, yB),
-                          (0, 255, 0), 2)
-    
-    # Write the output video 
-    #out.write(frame.astype('uint8'))
+        # resizing for faster detection
+        frame = cv2.resize(frame, (640, 480))
+        # using a greyscale picture, also for faster detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # detect people in the image
+        # returns the bounding boxes for the detected objects
+        boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
 
-# When everything done, release the capture
-cap.release()
-# and release the output
-# out.release()
-# finally, close the window
-cv2.destroyAllWindows()
-cv2.waitKey(1)
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+        # ensure safe updates
+        with persons_detected.get_lock():
+            persons_detected.value = len(boxes)
+            #print("Persons detected:", persons_detected)
+
+        for (xA, yA, xB, yB) in boxes:
+            # display the detected boxes in the colour picture
+            cv2.rectangle(frame, (xA, yA), (xB, yB),
+                            (0, 255, 0), 2)
+        
+        # Write the output video 
+        #out.write(frame.astype('uint8'))
+
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            queue_tracker.stop_tracking()
+            process.terminate()
+            process.join()
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    # and release the output
+    # out.release()
+    # finally, close the window
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+
+# magic stuff 
+if __name__ == "__main__":
+    multiprocessing.set_start_method("spawn")
+    main()
--- queue_calculator.py
+++ queue_calculator.py
@@ -1,26 +1,35 @@
 import time
 
-from human_detection import persons_detected
+# from human_detection import persons_detected
 
-run_timer = False            # turns the analyzer on
-timer = 0                    # keeps track of time in seconds
-refreshes_per_second = 0     # frequency of refreshes
+# persons_detected_prev = persons_detected # variable to compare new number of persons detected
 
-    # starts tracking per refresh how many people are in queue #
-def start_timer(refreshes_per_second): # takes in refresh rate of calculator
-    run_timer = True
-    time_per_refresh = 1 / refreshes_per_second
-    
-    while run_timer:
-        estimated_queue_time(persons_detected) # runs estimater with timer
-        time.sleep(time_per_refresh)
+class Queue_Calculator:
+    # run_timer = False            # turns the analyzer on
+    # timer = 0                    # keeps track of time in seconds
+    # refreshes_per_second = 0      # frequency of refreshes
 
-    # stops tracking people in line #
-def stop_timer():          
-    run_timer = False
+    def __init__(self, refreshes, time_per_person):
+        self.refreshes_per_second = refreshes
+        self.run_timer = False
+        self.time_per_person = time_per_person # idk minutes
 
-    # calculates estimated queue time #
-def estimated_queue_time(persons): 
-    p = persons
-    
-    # needs to calculate average time per person to estimate queue time
+        # calculates estimated queue time #
+    def estimated_queue_time(self, persons_detected): 
+        # needs to calculate average time per person to estimate queue time
+
+        return self.time_per_person * persons_detected.value
+
+        # starts tracking per refresh how many people are in queue #
+    def start_tracking(self, persons_detected): # takes in refresh rate of calculator
+        self.run_timer = True
+        time_per_refresh = 1 / self.refreshes_per_second
+        
+        while self.run_timer:
+            queue_time = self.estimated_queue_time(persons_detected) # runs estimater with timer
+            print("Estimated queue time:", queue_time)
+            time.sleep(time_per_refresh)
+
+        # stops tracking people in line #
+    def stop_tracking(self):          
+        self.run_timer = False
