ffe365f85ae022139324b447f1106446dfc30471
Jamesrsh8 <144559513+Jamesrsh8@users.noreply.github.com>
2025-03-29 14:14:49 -0500

    Fixed queue_calculator after merge conflicts

--- back_end/queue_calculator.py
+++ back_end/queue_calculator.py
@@ -1,4 +1,5 @@
 import time
+from back_end.human_detection import persons_detected, FRAMERATE
 
 # from human_detection import persons_detected
 
@@ -8,24 +9,12 @@ class Queue_Calculator:
     # run_timer = False            # turns the analyzer on
     # timer = 0                    # keeps track of time in seconds
     # refreshes_per_second = 0      # frequency of refreshes
-from back_end.human_detection import persons_detected, FRAMERATE
-
-run_timer = False            # turns the analyzer on
-timer = 0                    # keeps track of time in seconds
-
-    # starts tracking per refresh how many people are in queue #
-def start_timer(): # takes in refresh rate of calculator
-    run_timer = True
-    time_per_refresh = 1 / FRAMERATE
-    
-    while run_timer:
-        estimated_queue_time(persons_detected) # runs estimater with timer
-        time.sleep(time_per_refresh)
 
     def __init__(self, refreshes, time_per_person):
         self.refreshes_per_second = refreshes
         self.run_timer = False
         self.time_per_person = time_per_person # idk minutes
+        self.queue_time = 0
 
         # calculates estimated queue time #
     def estimated_queue_time(self, persons_detected): 
@@ -39,20 +28,14 @@ def start_timer(): # takes in refresh rate of calculator
         time_per_refresh = 1 / self.refreshes_per_second
         
         while self.run_timer:
-            queue_time = self.estimated_queue_time(persons_detected) # runs estimater with timer
-            print("Estimated queue time:", queue_time)
+            self.queue_time = self.estimated_queue_time(persons_detected) # runs estimater with timer
+            print("Estimated queue time:", self.queue_time)
             time.sleep(time_per_refresh)
 
-        # stops tracking people in line #
+    # stops tracking people in line #
     def stop_tracking(self):          
         self.run_timer = False
-    # calculates estimated queue time #
-def estimated_queue_time(persons): 
-    queue_time = persons
-    
-    return queue_time
     
     # needs to calculate average time per person to estimate queue time
-    
-def get_queue_time(): # allows the server to get the queue time
-    return estimated_queue_time(persons_detected)
+    def get_queue_time(): # allows the server to get the queue time
+        return self.queue_time
